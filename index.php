b7c8c2d1a4bc438ca35fefcd70308dddaf911b5e
Rhea Bonifacio <rhea.bonifacio@blueprintsolutionsgroup.com>
2015-02-10 21:16:18 +0800

    autoloaders class has been added

--- index.php
+++ index.php
@@ -1,4 +1,11 @@
 <?php
+/**
+ * The SuperClass for WonderWoman with the application of OOP concepts and principkes
+ *
+ * author: Rhea A. Bonifacio
+ * version: 1.0
+ * date: 2015 Feb 9
+ */
 
 interface IHuman {
 	function fetchName();
@@ -23,4 +30,5 @@ $womanObj->lastname = 'Bonifacio';
 $womanObj->middlename = 'Angeles';
 print_r( $womanObj->fetchName());
 
+/* End of file: index.php */
 ?>
